9f257f68301c57816bc90e071f646ba77adf4b11
abhinnabiswal <40041820+abhinnabiswal@users.noreply.github.com>
2019-03-11 01:13:01 +0530

    Create Hello-World.c

---
+++ Hello-World.c
@@ -0,0 +1,11 @@
+%  A simple C program to display "Hello, World!" on the screen. 
+%  Since, it's a very simple program, it is often used to illustrate 
+%  the syntax of a programming language.
+
+#include <stdio.h>
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
